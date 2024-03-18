f76f93d2be4dc9fe33f4cb05389357886eb961d8
Aychin Gasimov <aychin@amazon.com>
2024-03-18 15:15:48 +0100

    build.sh adjusted

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p ./portable
-cp pireporterPolicy.json baselineNetworkPerformance.json report.css ./portable
+cp pireporterPolicy.json report.css ./portable
 pkg pireporter.js -t node16-linux --out-path ./portable
