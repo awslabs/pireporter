c756268e816fe49ef3470a39ead9a8c4024ec2b8
Aychin Gasimov <aychin@amazon.com>
2024-06-06 20:02:04 +0200

    Update build file

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p ./portable
-cp pireporterPolicy.json report.css ./portable
+cp conf.json pireporterPolicy.json report.css ./portable
 pkg pireporter.js -t node16-linux --out-path ./portable
