25d7017f6808bcf969e73586cf28f1787ee8b6f2
Aychin <aychin@amazon.com>
2026-01-22 14:14:36 +0100

    Introducing comprehensive chat mode for AI analyzes with MCP support.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 mkdir -p ./portable
 cp conf.json pireporterPolicy.json report.css ./portable
 cp -r genai ./portable/
-pkg pireporter.js -t node16-linux --out-path ./portable
+pkg pireporter.js -t node18-linux --out-path ./portable
