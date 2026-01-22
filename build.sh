eff7b4db387a6b33ecea42ccb15d4703ce1ca545
Aychin <aychin@amazon.com>
2026-01-22 14:18:29 +0100

    Switched to maintained @yao-pkg/pkg

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# Using @yao-pkg/pkg - the maintained fork of vercel/pkg
+# Install with: npm install -g @yao-pkg/pkg
+
 mkdir -p ./portable
 cp conf.json pireporterPolicy.json report.css ./portable
 cp -r genai ./portable/
-pkg pireporter.js -t node18-linux --out-path ./portable
+pkg pireporter.js -t node20-linux --out-path ./portable
